1d39e7a4db20ea461b25f1ce160c2203a900a8a3
Mukuma Musenge <kuma.musenge@gmail.com>
2019-08-23 13:12:54 +0200

    Initial Commit - Fizz Buzz Challenge Code using Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  	<div id="app">
+    	<b-card
+      		title="FizzBuzz Challenge Using Vue"
+      		sub-title="Simple application of the fizz buzz challenge using Vue.js"
+		>
+			<b-card-text>
+				<b-list-group>
+					<b-list-group-item v-if="!items.length">No Data Yet</b-list-group-item>
+					<b-list-group-item v-for="item in items" v-bind:key="item.id">{{ item.message }}</b-list-group-item>
+				</b-list-group>
+			</b-card-text>
+			<b-button variant="outline-primary" v-on:click="runCheck()">Check</b-button>
+   		</b-card>
+  	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const defaultValue = 100;
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  	name: "app",
+  	data() {
+		return {
+			items: []
+	};
+},
+methods: {
+	runCheck: function() {
+		this.items = []
+		for (let index = 1; index <= defaultValue; index++) {
+			if (index % 3 === 0 && index % 5 === 0) {
+				this.addToArray(index, "FizzBuzz");
+			} else if (index % 3 == 0) {
+				this.addToArray(index, "Fizz");
+			} else if (index % 5 == 0) {
+				this.addToArray(index, "Buzz");
+			} else {
+				this.addToArray(index, index);
+			}
+		}
+		},
+		addToArray: function(index, message) {
+			this.items.push({
+				id: index,
+				message: message
+			});
+    	}
+  	}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
